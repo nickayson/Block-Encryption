ea36d94371eb46db6da8eee5c449a71215fd1532
AJ <ajalbrechttwin@gmail.com>
2021-10-07 16:44:53 -0700

    Co-authored-by: Wu, Po-Tyng <gaidepeter@csu.fullerton.edu> Co-authored-by: Darrick Rusk <drusk1@users.noreply.github.com> Co-authored-by: Nicholas Ayson <nickayson@users.noreply.github.com>

--- Block Encryption actual/cs-sketch.js
+++ Block Encryption actual/cs-sketch.js
@@ -17,7 +17,7 @@ function setup() // P5 Setup Fcn
     let width = sz * g_canvas.wid;  // Our 'canvas' uses cells of given size, not 1x1 pixels.
     let height = sz * g_canvas.hgt;
     createCanvas( width, height );  // Make a P5 canvas.
-    draw_grid( 10, 50, 'white', 'yellow' );
+    // draw_grid( 10, 50, 'white', 'yellow' );
 
     // Setup input-box for input and a callback fcn when button is pressed.
     g_input_1 = createInput( ); // Create an input box, editable.
@@ -30,9 +30,75 @@ function setup() // P5 Setup Fcn
 // Callback to get Input-box data.
 function retrieve_input_1()
 {
-    var data = g_input_1.value(); // Get data from Input box.
-    console.log( "data = " + data ); // Show data in F12 Console output.
+    data = g_input_1.value(); // Get data from Input box.
+    var index = 0;
+    var array_of_characters = new Array();
+    let newArray = data.split(''); // splits every letter in string into an item in our array
+    var upper_case = false;
+    var lower_case = false;
+    var symbol =     false;
+    while (index != data.length) {
+        //var ascii_code = data;
+        var i = data.charCodeAt(index); // this is the number at the ascii reference
+        console.log( "data = " + i); // Show data in F12 Console output.
+        array_of_characters[index] = i;
+        console.log( "array data - " + array_of_characters[index])
+        // upper case check
+        if (array_of_characters[index] >= 65 && array_of_characters[index] <= 90) {
+            upper_case = true;
+            console.log("is upper case");
+        } else {
+            console.log("value is not uppercase")
+        }
+        // lower case check
+        if (array_of_characters[index] >= 97 && array_of_characters[index] <= 122) {
+            lower_case = true;
+            console.log("is lower case");
+        } else {
+            console.log("value is not lowercase")
+        }
+        // symbol check
+        if ((array_of_characters[index] >= 33 && array_of_characters[index] <= 47) || 
+            (array_of_characters[index] >= 91 && array_of_characters[index] <= 96) || 
+            (array_of_characters[index] >= 123 && array_of_characters[index] <= 127) ) {
+            symbol = true;
+            console.log("is a symbol");
+        } else {
+            console.log("value is not a symbol");
+        }
+        
+        index++;
+    }
+    // Password must have at least eight characters including an uppercase and lowercase letter, a symbol, and a digit. It may not contain a
+    // dictionary word (comprehensive8).
+    if (array_of_characters.length != 8) {
+        console.log ( "the password is not the right length"); // password is a fixed 8 characters
+    }
 }
+//"ABC".charCodeAt(0) // returns 65
+// String.fromCharCode(65,66,67); // returns 'ABC'
+// {
+//     "31": "",      "32": " ",     "33": "!",     "34": "\"",    "35": "#",    
+//     "36": "$",     "37": "%",     "38": "&",     "39": "'",     "40": "(",    
+//     "41": ")",     "42": "*",     "43": "+",     "44": ",",     "45": "-",    
+//     "46": ".",     "47": "/",     "48": "0",     "49": "1",     "50": "2",    
+//     "51": "3",     "52": "4",     "53": "5",     "54": "6",     "55": "7",    
+//     "56": "8",     "57": "9",     "58": ":",     "59": ";",     "60": "<",    
+//     "61": "=",     "62": ">",     "63": "?",     "64": "@",     "65": "A",    
+//     "66": "B",     "67": "C",     "68": "D",     "69": "E",     "70": "F",    
+//     "71": "G",     "72": "H",     "73": "I",     "74": "J",     "75": "K",    
+//     "76": "L",     "77": "M",     "78": "N",     "79": "O",     "80": "P",    
+//     "81": "Q",     "82": "R",     "83": "S",     "84": "T",     "85": "U",    
+//     "86": "V",     "87": "W",     "88": "X",     "89": "Y",     "90": "Z",    
+//     "91": "[",     "92": "\\",    "93": "]",     "94": "^",     "95": "_",    
+//     "96": "`",     "97": "a",     "98": "b",     "99": "c",     "100": "d",    
+//     "101": "e",    "102": "f",    "103": "g",    "104": "h",    "105": "i",    
+//     "106": "j",    "107": "k",    "108": "l",    "109": "m",    "110": "n",    
+//     "111": "o",    "112": "p",    "113": "q",    "114": "r",    "115": "s",    
+//     "116": "t",    "117": "u",    "118": "v",    "119": "w",    "120": "x",    
+//     "121": "y",    "122": "z",    "123": "{",    "124": "|",    "125": "}",    
+//     "126": "~",    "127": ""
+//     }
 
 // // Past this point is the black grid
 // // Globals to keep track of Bot
@@ -135,4 +201,4 @@ function retrieve_input_1()
 //     g_bot.y %= g_box.hgt;
 //     //console.log( "bot x,y = " + g_bot.x + "," + g_bot.y );
 //     draw_bot( );
-}
+// }
